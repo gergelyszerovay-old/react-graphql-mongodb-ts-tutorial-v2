7d3fbfa84a431cc7d6c64bc22942d03e0eaea2d7
Gergely Szerovay <gergely@szerovay.hu>
2020-06-10 17:11:54 +0200

    Sign In form e2e tests

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -12,10 +12,10 @@ export const TopMenu: FC<TopMenuProps> = ({selected}: TopMenuProps) => {
     return (
         <Header>
             <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[selected]}>
-                <Menu.Item key="notes">Note List<Link to="/notes"/></Menu.Item>
-                <Menu.Item key="newnote">New Note<Link to="/newnote"/></Menu.Item>
-                <Menu.Item key="logout">Logout<Link to="/"/></Menu.Item>
-                <Menu.Item key="debug">Debug<Link to="/debug"/></Menu.Item>
+                <Menu.Item key="notes" id="app-top-menu-notes">Note List<Link to="/notes"/></Menu.Item>
+                <Menu.Item key="newnote" id="app-top-menu-newnote">New Note<Link to="/newnote"/></Menu.Item>
+                <Menu.Item key="logout" id="app-top-menu-logout">Logout<Link to="/"/></Menu.Item>
+                <Menu.Item key="debug" id="app-top-menu-debug">Debug<Link to="/debug"/></Menu.Item>
             </Menu>
         </Header>
     )
--- src/components/TopMenuSign.tsx
+++ src/components/TopMenuSign.tsx
@@ -12,9 +12,9 @@ export const TopMenuSign: FC<TopMenuSignProps> = ({selected}: TopMenuSignProps) 
     return (
         <Header>
             <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[selected]}>
-                <Menu.Item key="signin">Sign In<Link to="/"/></Menu.Item>
-                <Menu.Item key="signup">Sign Up<Link to="/signup"/></Menu.Item>
-                <Menu.Item key="debug">Debug<Link to="/debug"/></Menu.Item>
+                <Menu.Item key="signin" id="app-top-menu-signin">Sign In<Link to="/"/></Menu.Item>
+                <Menu.Item key="signup" id="app-top-menu-signup">Sign Up<Link to="/signup"/></Menu.Item>
+                <Menu.Item key="debug" id="app-top-menu-debug">Debug<Link to="/debug"/></Menu.Item>
             </Menu>
         </Header>
     )
